9285c70ac6c469755ad22ba902030a1dec93f2e5
Renato <rena.f.oliveira@gmail.com>
2020-09-12 22:36:27 -0300

    Criando animação no CSS e conclusão de projeto

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 let isJumping = false;
+let position = 0;
 
 
 function handleKeyUp(event) {
@@ -12,18 +13,18 @@ function handleKeyUp(event) {
 }
 
 function jump(){
-    let position = 0;
+
     isJumping = true;
 
     let upInternval = setInterval(() => {
         if (position >= 150) {
             clearInterval(upInternval);
-            isJumping = false;
 
             //Descendo
             let downInterval = setInterval(() => {
                 if (position <= 0){
                     clearInterval(downInterval);
+                    isJumping = false;
                 }else {
                     position -= 20;
                 dino.style.bottom = position + 'px';
@@ -40,12 +41,31 @@ function jump(){
 
 function createCactus() {
     const cactus = document.createElement('div');
-    let cactusPosition = 1000;
+    let cactusPosition = 1920;
+    let randomTime = Math.random() * 6000;
     
     cactus.classList.add('cactus');
     cactus.style.left = 1000 + 'px';
     background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        
+        if (cactusPosition < -60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        }else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            //Game Over
+
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class = "game-over">Fim de jogo</h1>'
+        }else{
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
 }
 
 createCactus();
-document.addEventListener('keyup', hadleKeyUp);
+document.addEventListener('keyup', handleKeyUp);
